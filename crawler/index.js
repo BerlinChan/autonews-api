1d82c3b5c1235fda2a525298c2f61866ba4e5a1c
Berlin Chan <berlin.chen@okchem.com>
2017-03-08 17:47:38 +0800

    edit config

--- crawler/index.js
+++ crawler/index.js
@@ -1,6 +1,6 @@
 /**
  * Created by berlin on 2017/3/6.
- *
+ * crawler task runner
  */
 
 const dachu_news_crawler = require("./dachu_news");
@@ -11,10 +11,10 @@ const ctjb_crawler = require("./ctjb");
 const ctkb_crawler = require("./ctkb");
 const ctsb_crawler = require("./ctsb");
 
-//dachu_news_crawler();
-// sxwb_crawler();
-// ctdsb_crawler();
-//hbrb_crawler();
-//ctjb_crawler();
-//ctkb_crawler();
+dachu_news_crawler();
+sxwb_crawler();
+ctdsb_crawler();
+hbrb_crawler();
+ctjb_crawler();
+ctkb_crawler();
 ctsb_crawler();
