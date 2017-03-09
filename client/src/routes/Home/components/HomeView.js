ecea334263be8826cd60ea3281486bf256bb2baf
Berlin Chan <berlin.chen@okchem.com>
2017-03-09 16:44:46 +0800

    prepare home

--- client/src/routes/Home/components/HomeView.js
+++ client/src/routes/Home/components/HomeView.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import cls from './HomeView.scss'
 
-const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    Scroll down to see the bottom right gray button.
-  </div>);
+class HomeView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={cls.home}>
+        <h4>Welcome!</h4>
+        Scroll down to see the bottom right gray button.
+      </div>
+    );
+  }
+
+}
 
 export default HomeView
