c5f97b798d0507e38fe7f8561799c0341b281832
Berlin Chan <berlin.chen@okchem.com>
2017-03-13 10:01:11 +0800

    deploy prod 0.0.3

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -2,5 +2,5 @@ export default  {
   labelSpan: "8",
   wrapperSpan: "16",
   oAuth: 'http://localhost:3090/login',
-  API_SERVER: 'http://localhost:3090',// production- http://autonews.iok.la:22742/
+  API_SERVER: 'http://autonews.iok.la:22742/',// production- http://autonews.iok.la:22742/
 }
