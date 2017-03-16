7122434afeb6a665b722fd423385ad2e2e7c1966
Berlin Chan <berlin.chen@okchem.com>
2017-03-16 18:13:06 +0800

    将firebase框架从项目中移除 firebase database不支持多条件查询，而且速度成问题，将来保存、查询大量的新闻列表数据会成问题 尝试使用mongoDB替代
    WIP: 添加排重逻辑, list item, detail item, origin item 之间的关联查询

--- src/crawler/dbConnection.js
+++ src/crawler/dbConnection.js
@@ -4,20 +4,22 @@
  */
 
 const MongoClient = require('mongodb').MongoClient;
+const config = require('../utils/config');
 
-// Connection URL
-const url = 'mongodb://localhost:27017/auto-news';
+let db;
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function (err, db) {
-    console.log("Connected successfully to server");
-
-    findDocuments(db, function() {
-        db.close();
-    });
+MongoClient.connect(config.DB_SERVER, function (err, database) {
+    if (!err) {
+        console.log("Connected successfully to server");
+        db = database;
+    }
 });
 
-//test
+
+//test method
+
+//Insert a Document
 const insertDocuments = function (db, callback) {
     // Get the documents collection
     let collection = db.collection('documents');
@@ -29,7 +31,8 @@ const insertDocuments = function (db, callback) {
         callback(result);
     });
 };
-const findDocuments = function (db, callback) {
+//Find All Documents
+const findAllDocuments = function (db, callback) {
     // Get the documents collection
     let collection = db.collection('documents');
     // Find some documents
@@ -39,5 +42,42 @@ const findDocuments = function (db, callback) {
         callback(docs);
     });
 };
+//Find Documents with a Query Filter
+const findDocuments = function (db, callback) {
+    // Get the documents collection
+    let collection = db.collection('documents');
+    // Find some documents
+    collection.find({'a': 3}).toArray(function (err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs);
+        callback(docs);
+    });
+};
+//Update a document
+const updateDocument = function (db, callback) {
+    // Get the documents collection
+    let collection = db.collection('documents');
+    // Update document where a is 2, set b equal to 1
+    collection.updateOne({a: 2}
+        , {$set: {b: 1}}, function (err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            console.log("Updated the document with the field a equal to 2");
+            callback(result);
+        });
+};
+//Remove a document
+const removeDocument = function (db, callback) {
+    // Get the documents collection
+    let collection = db.collection('documents');
+    // Delete document where a is 3
+    collection.deleteOne({a: 3}, function (err, result) {
+        assert.equal(err, null);
+        assert.equal(1, result.result.n);
+        console.log("Removed the document with the field a equal to 3");
+        callback(result);
+    });
+};
 
-//module.exports = admin.database();
+module.exports = db;
