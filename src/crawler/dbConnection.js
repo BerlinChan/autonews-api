21b99ab4f5b78ad2c83f6070d80ad53d8e31721e
Berlin Chan <berlin.chen@okchem.com>
2017-03-16 17:41:07 +0800

    将firebase框架从项目中移除 firebase database不支持多条件查询，而且速度成问题，将来保存、查询大量的新闻列表数据会成问题 尝试使用mongoDB替代
    WIP: 添加排重逻辑, list item, detail item, origin item 之间的关联查询

--- src/crawler/dbConnection.js
+++ src/crawler/dbConnection.js
@@ -3,17 +3,41 @@
  * Crawler Constructor
  */
 
-const admin = require("firebase-admin");
-const serviceAccount = require("../utils/fiery-heat-7406-firebase-adminsdk-kpbna-ed5b591529.json");
+const MongoClient = require('mongodb').MongoClient;
 
-// Initialize the app with a custom auth variable, limiting the server's access
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://fiery-heat-7406.firebaseio.com/",
-    databaseAuthVariableOverride: {
-        uid: "crawler-worker",
-    },
+// Connection URL
+const url = 'mongodb://localhost:27017/auto-news';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function (err, db) {
+    console.log("Connected successfully to server");
+
+    findDocuments(db, function() {
+        db.close();
+    });
 });
 
-// Get a database reference to our blog
-module.exports = admin.database();
+//test
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    let collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        {a: 1}, {a: 2}, {a: 3}
+    ], function (err, result) {
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
+const findDocuments = function (db, callback) {
+    // Get the documents collection
+    let collection = db.collection('documents');
+    // Find some documents
+    collection.find({}).toArray(function (err, docs) {
+        console.log("Found the following records");
+        console.log(docs);
+        callback(docs);
+    });
+};
+
+//module.exports = admin.database();
