ca51a429ba95070c6cf9aa7bdc02e1a82c63285f
Berlin Chan <berlin.chen@okchem.com>
2017-03-18 01:38:27 +0800

    db init script
    WIP: list item, detail item, origin item 之间的关联查询

--- src/crawler/dbInit.js
+++ src/crawler/dbInit.js
@@ -11,13 +11,13 @@ const db = monk(config.DB_SERVER);
 //database init
 db.get('origin').insert(
     [
-        {key: 'ctdsb', name: '楚天都市报',},
-        {key: 'hbrb', name: '湖北日报',},
-        {key: 'sxwb', name: '三峡晚报',},
-        {key: 'ctkb', name: '楚天快报',},
-        {key: 'ctjb', name: '楚天金报',},
-        {key: 'txdcw', name: '腾讯大楚网',},
-        {key: 'ctsb', name: '楚天时报',}
+        {"_id": monk.id("58cb5e1237e1e826b4530627"), "key": "ctdsb", "name": "楚天都市报"},
+        {"_id": monk.id("58cb5e1237e1e826b4530628"), "key": "hbrb", "name": "湖北日报"},
+        {"_id": monk.id("58cb5e1237e1e826b4530629"), "key": "sxwb", "name": "三峡晚报"},
+        {"_id": monk.id("58cb5e1237e1e826b453062a"), "key": "ctkb", "name": "楚天快报"},
+        {"_id": monk.id("58cb5e1237e1e826b453062b"), "key": "ctjb", "name": "楚天金报"},
+        {"_id": monk.id("58cb5e1237e1e826b453062c"), "key": "txdcw", "name": "腾讯大楚网"},
+        {"_id": monk.id("58cb5e1237e1e826b453062d"), "key": "ctsb", "name": "楚天时报"},
     ]
 );
 db.create("requestedUrl", {capped: true, size: 209715200});//记录已请求url collection，大小：200MB
