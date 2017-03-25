ae9d37157cb15cef00d6211af9ad998adb2bf228
Berlin Chan <berlin.chen@okchem.com>
2017-03-25 10:46:38 +0800

    FE: upgrade ant design to 2.8.2
    WIP: 其他crawler option

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = new Koa();
 const io = new IO();
 app.use(compress({
     filter: function (content_type) {
-        return /text/i.test(content_type)
+        return /[text|json|javascript]/i.test(content_type)
     },
     threshold: 2048,
     flush: require('zlib').Z_SYNC_FLUSH,
