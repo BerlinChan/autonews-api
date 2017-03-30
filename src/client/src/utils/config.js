59fc6bfe51a8f6b4d52219013cb74e7f30d187d6
Berlin Chan <berlin.chen@okchem.com>
2017-03-30 15:56:53 +0800

    release 0.2.0
    WIP: 其他crawler option

--- src/client/src/utils/config.js
+++ src/client/src/utils/config.js
@@ -1,4 +1,4 @@
 export default  {
   oAuth: 'http://localhost:3090/login',
-  API_SERVER: 'http://localhost:3090/',// production- http://autonews.iok.la:22742/
+  API_SERVER: 'http://autonews.iok.la:22742/',// production- http://autonews.iok.la:22742/
 }
