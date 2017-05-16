6c0c337401034122bff9f5d7583b76ae187773ae
Berlin Chan <berlin.chen@okchem.com>
2017-05-16 10:21:59 +0800

    edit config

--- server/DAO.js
+++ server/DAO.js
@@ -36,18 +36,6 @@ async function getTodayList(origin_key) {
     );
 }
 
-//按(开始时间: date，结束时间: date，origin_key: string)查询list
-function getSpecificList(beginDate = new Date(moment().format('YYYY-MM-DD')), endDate = new Date(moment().add({days: 1}).format('YYYY-MM-DD')), origin_key = 'ctdsb,ctjb,ctkb,ctsb,txdcw,hbrb,sxwb') {
-    let origin_key_array = origin_key.split(',');
-    return db.get('list').find(
-        {
-            "date": {$gte: new Date(Date.parse(beginDate) - 57600000), $lt: new Date(Date.parse(endDate) - 57600000)}, //减去16小时？
-            "origin_key": {$in: origin_key_array}
-        },
-        {sort: {'date': -1}, fields: '-origin_name'}
-    );
-}
-
 /*
  * 查询往期数据
  *
@@ -62,7 +50,10 @@ function getSpecificList(beginDate = new Date(moment().format('YYYY-MM-DD')), en
 async function pastInquiry(origin = '', beginDate, endDate, keyword = '', current = 1, pageSize = 20) {
     let origin_key_array = origin.split(',');
     let query = {
-        "date": {$gte: new Date(Date.parse(beginDate) - 28800000), $lt: new Date(Date.parse(endDate) - 28800000)}, //减去8小时？
+        "date": {
+            $gte: new Date(Date.parse(beginDate) - 28800000),
+            $lt: new Date(Date.parse(endDate) - 28800000)
+        }, //减去8小时？
         "origin_key": {$in: origin_key_array},
     };
     if (keyword) {
